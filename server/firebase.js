7634debd534585d9ec7fbdf498d72dab48501638
Ibrahim Cheena <ibrahimcheena18@gmail.com>
2025-04-05 13:58:36 -0700

    Initial commit of full-stack Event Insurance Platform

--- server/firebase.js
+++ server/firebase.js
@@ -1,8 +1,17 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
 
-const serviceAccount = require("./firebase-admin-sdk.json");
+dotenv.config();
+
+// Get the path from environment variable
+const serviceAccountPath = process.env.FIREBASE_CONFIG_PATH;
+
+// Read and parse the JSON file from the path
+const serviceAccount = JSON.parse(
+  fs.readFileSync(path.resolve(serviceAccountPath), "utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

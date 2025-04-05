b843efdaddcfc5ba2677d9c68579321eb50907cf
Ibrahim Cheena <ibrahimcheena18@gmail.com>
2025-04-05 14:27:09 -0700

    Initial clean commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeVwNOyMDHHeWT1E1G5zjJQz_dyEzWiX8",
-    authDomain: "event-insurance-platform.firebaseapp.com",
-    projectId: "event-insurance-platform",
-    storageBucket: "event-insurance-platform.firebasestorage.app",
-    messagingSenderId: "187863786700",
-    appId: "1:187863786700:web:e24bda70e3d54a6bab8e69",
-    measurementId: "G-BT7BL835Y9"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
